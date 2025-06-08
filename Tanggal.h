7a1990cec99374fdefd7864a2ec08a94d84a56cd
Sabinaatha10 <sabinaathaa@gmail.com>
2025-06-08 17:24:36 +0700

    Header Tanggal ver final

--- Tanggal.h
+++ Tanggal.h
@@ -3,14 +3,13 @@
 
 #include <time.h>
 
-// Deklarasi variabel global untuk tanggal saat ini
-// 'extern' menandakan bahwa variabel ini didefinisikan di file .c lain
+// Variabel global untuk tanggal saat ini
+// 'extern' karena variabel ini didefinisikan di file .c lain
 extern struct tm tanggalSekarang;
 
-// Prototipe fungsi
 void konversiKeTanggal(const char *tanggalStr, struct tm *date);
 void formatTanggal(char *buffer, struct tm date);
 void simpanTanggalSaatIni(struct tm date);
 int bacaTanggalSaatIni(struct tm *date);
 
-#endif // TANGGAL_H
+#endif 
